6fd8d0b405709899a61eaea5331d7d367c79888c
Ali bin shoaib <Ali.h.bin.shoaib@gmail.com>
2023-10-17 21:50:55 +0300

    add media query for screen <1140px

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,9 @@ let slider = document.getElementsByClassName('slide-container ')[0];
 let cards = document.getElementsByClassName('dish-card');
 let rightBtn = document.getElementsByClassName('fa-angle-right')[0];
 let leftBtn = document.getElementsByClassName('fa-angle-left')[0];
+
 let gap = 48;
-let cardWidth = cards[0].clientWidth + gap;
+
 let n = 1;
 rightBtn.addEventListener('click', () => {
 	if (n >= cards.length - 2) {
@@ -13,11 +14,13 @@ rightBtn.addEventListener('click', () => {
 		leftBtn.classList.remove('bg-color-primary', 'text-color-light');
 		rightBtn.classList.add('bg-color-primary', 'text-color-light');
 
-		transformSlider(-n * cardWidth);
+		transformSlider(-n * getCardWidth());
 		n++;
 	}
 });
 leftBtn.addEventListener('click', () => {
+	let cardWidth = cards[0].clientWidth + gap;
+
 	if (n === 1) {
 	} else {
 		rightBtn.classList.remove('bg-color-primary', 'text-color-light');
@@ -25,10 +28,13 @@ leftBtn.addEventListener('click', () => {
 		leftBtn.classList.remove('bg-color-gray');
 		leftBtn.classList.add('bg-color-primary', 'text-color-light');
 
-		transformSlider(-(n - 1) * cardWidth + cardWidth);
+		transformSlider(-(n - 1) * getCardWidth() + getCardWidth());
 		n--;
 	}
 });
 function transformSlider(direction) {
 	slider.style.transform = `translateX(${direction}px)`;
+}
+function getCardWidth() {
+	return cards[0].clientWidth + gap;
 }
