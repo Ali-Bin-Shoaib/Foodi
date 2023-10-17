e6976965ec9f2194eb1020c5175284de7f514f07
Ali bin shoaib <Ali.h.bin.shoaib@gmail.com>
2023-10-17 14:05:47 +0300

    add hover effect on links and icons and slider controls

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,34 @@
-// let slider = document.getElementsByClassName('slide-container ')[0];
-// let cards = document.getElementsByClassName('dish-card');
-// let rightBtn = document.getElementsByClassName('fa-angle-right')[0];
-// let leftBtn = document.getElementsByClassName('fa-angle-left')[0];
-// let gap = 48;
-// let cardWidth = cards[0].clientWidth + gap;
-// let n = 1;
-// rightBtn.addEventListener('mouseover', () => {
-// 	if (n >= cards.length - 2) {
-// 	} else {
-// 		rightBtn.classList.remove('bg-color-gray');
-// 		leftBtn.classList.add('bg-color-gray');
-// 		leftBtn.classList.remove('bg-color-primary', 'text-color-light');
-// 		rightBtn.classList.add('bg-color-primary', 'text-color-light');
+let slider = document.getElementsByClassName('slide-container ')[0];
+let cards = document.getElementsByClassName('dish-card');
+let rightBtn = document.getElementsByClassName('fa-angle-right')[0];
+let leftBtn = document.getElementsByClassName('fa-angle-left')[0];
+let gap = 48;
+let cardWidth = cards[0].clientWidth + gap;
+let n = 1;
+rightBtn.addEventListener('click', () => {
+	if (n >= cards.length - 2) {
+	} else {
+		rightBtn.classList.remove('bg-color-gray');
+		leftBtn.classList.add('bg-color-gray');
+		leftBtn.classList.remove('bg-color-primary', 'text-color-light');
+		rightBtn.classList.add('bg-color-primary', 'text-color-light');
 
-// 		transformSlider(-n * cardWidth);
-// 		n++;
-// 	}
-// });
-// leftBtn.addEventListener('mouseover', () => {
-// 	if (n === 1) {
-// 	} else {
-// 		rightBtn.classList.remove('bg-color-primary', 'text-color-light');
-// 		rightBtn.classList.add('bg-color-gray');
-// 		leftBtn.classList.remove('bg-color-gray');
-// 		leftBtn.classList.add('bg-color-primary', 'text-color-light');
+		transformSlider(-n * cardWidth);
+		n++;
+	}
+});
+leftBtn.addEventListener('click', () => {
+	if (n === 1) {
+	} else {
+		rightBtn.classList.remove('bg-color-primary', 'text-color-light');
+		rightBtn.classList.add('bg-color-gray');
+		leftBtn.classList.remove('bg-color-gray');
+		leftBtn.classList.add('bg-color-primary', 'text-color-light');
 
-// 		transformSlider(-(n - 1) * cardWidth + cardWidth);
-// 		n--;
-// 	}
-// });
-// function transformSlider(direction) {
-// 	slider.style.transform = `translateX(${direction}px)`;
-// }
+		transformSlider(-(n - 1) * cardWidth + cardWidth);
+		n--;
+	}
+});
+function transformSlider(direction) {
+	slider.style.transform = `translateX(${direction}px)`;
+}
